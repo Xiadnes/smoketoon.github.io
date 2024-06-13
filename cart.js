01506dfdbf318ab334add1c2a320029ddcb55bec
Xiadnes <86753170+Xiadnes@users.noreply.github.com>
2024-06-13 22:59:05 +0300

    Add files via upload

--- cart.js
+++ cart.js
@@ -1,32 +1,35 @@
-const cardSide = document.querySelectorAll('.cart')
+const cardSide = document.querySelectorAll('.knopki')
 const storage = JSON.parse(localStorage.getItem('but') || '[]')
-const cart = document.querySelectorAll('.cart')
-
+const cart = document.querySelectorAll('.knopki')
+const ofor = document.createElement('button')
+ofor.innerHTML = 'Підтвердити замовлення'
+ofor.className = 'ofor';
 var count = 0
 if (storage.length) {
 	storage.forEach(el => {
 		const newCard = document.createElement('div')
-		newCard.innerHTML = '<p>'+el.title+'</p><button id="'+count+'">Delete</button>'
+		newCard.className = 'foo';
+		let o = JSON.parse(localStorage.getItem('but'));
+		console.log(o)
+		newCard.innerHTML = '<p>'+el.title+'</p><button id="'+count+'">Видалити</button>'
 		cardSide[0].appendChild(newCard)
 		cart.forEach((el, idx) =>{
 			const btn = el.childNodes[count].childNodes[1]
 			const elik = cart[0].childNodes[btn.id]
-			console.log(btn)
 			btn.addEventListener('click', () =>{
 				cardSide[0].removeChild(elik)
 				let o = JSON.parse(localStorage.getItem('but'));
-				console.log(btn.id)
 				o.splice(btn.id,1);
-				console.log(o)
 				localStorage.setItem('but', JSON.stringify(o));
 				location.reload()
   });
 });
 		count = count+1
 });
+cardSide[0].appendChild(ofor)
 }
 
-
+//'<img src="'+el.title+'.png" alt="">'+'
 
 
 
--- script.js
+++ script.js
@@ -5,7 +5,7 @@ tg.expand();
 
 let item = "";
 
-
+const storage = localStorage.getItem('but') || '[]'
 buttons.forEach((el, idx) =>{
 
   const title = el.childNodes[0].innerText
@@ -13,12 +13,17 @@ buttons.forEach((el, idx) =>{
   const price = 2
   
   btn.addEventListener('click', () =>{
-    const storage = localStorage.getItem('but') || '[]'
-    const cart = JSON.parse(storage)
-    const card = {title}
-    localStorage.setItem('but', JSON.stringify([...cart,card]))
-    item = title;
-    tg.sendData(item);
+    let o = JSON.parse(localStorage.getItem('but'));
+    if (o.length < 7) {
+        const storage = localStorage.getItem('but') || '[]'
+        const cart = JSON.parse(storage)
+        const card = {title}
+        localStorage.setItem('but', JSON.stringify([...cart,card]))
+//      item = title;
+//      tg.sendData(item);
+    } else {
+      
+    }
   })
 
 
