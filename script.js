68ec6b9eba008ce3faa8ec8985312d6d4d775add
Xiadnes <86753170+Xiadnes@users.noreply.github.com>
2024-07-15 02:32:48 +0300

    script.js

--- script.js
+++ script.js
@@ -19,15 +19,17 @@ buttons.forEach((el, idx) =>{
     btn.addEventListener('touchstart', () => {
     let o = JSON.parse(localStorage.getItem('but'));
     if (storage.length < 6) {
+    if (o.length < 6) {
         const storage = localStorage.getItem('but') || '[]'
         const cart = JSON.parse(storage)
         const card = {title}
         localStorage.setItem('but', JSON.stringify([...cart,card]))
-    }});
+    }}});
   } else{
     btn.addEventListener('click', () => {
     let o = JSON.parse(localStorage.getItem('but'));
     if (storage.length < 6) {
+    if (o.length < 6) {
         const storage = localStorage.getItem('but') || '[]'
         const cart = JSON.parse(storage)
         const card = {title}
