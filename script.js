286db010728bb00bf78ff9a988e8d1fcf316ab1a
Xiadnes <86753170+Xiadnes@users.noreply.github.com>
2024-07-15 00:24:21 +0300

    script.js

--- script.js
+++ script.js
@@ -5,50 +5,38 @@ tg.expand();
 
 let item = "";
 
+mobile = navigator.maxTouchPoints > 0 && 'orientation' in window
+console.log(mobile)
+
+
 const storage = localStorage.getItem('but') || '[]'
 buttons.forEach((el, idx) =>{
 
   const title = el.id
   const btn = el
   const price = 2
+  if (mobile === 'true') {
+    btn.addEventListener('touchstart', () => {
+    let o = JSON.parse(localStorage.getItem('but'));
+    if (o.length < 6) {
+        const storage = localStorage.getItem('but') || '[]'
+        const cart = JSON.parse(storage)
+        const card = {title}
+        localStorage.setItem('but', JSON.stringify([...cart,card]))
+    }});
+  } else{
+    btn.addEventListener('click', () => {
+    let o = JSON.parse(localStorage.getItem('but'));
+    if (o.length < 6) {
+        const storage = localStorage.getItem('but') || '[]'
+        const cart = JSON.parse(storage)
+        const card = {title}
+        localStorage.setItem('but', JSON.stringify([...cart,card]))
+    }});
+  };
   
-  btn.addEventListener('click', () =>{
-    let o = JSON.parse(localStorage.getItem('but'));
-    if (o.length < 6) {
-        const storage = localStorage.getItem('but') || '[]'
-        const cart = JSON.parse(storage)
-        const card = {title}
-        localStorage.setItem('but', JSON.stringify([...cart,card]))
-//      item = title;
-//      tg.sendData(item);
-    } else {
-      
-    }
-  })
 
-  btn.addEventListener("touchstart", () =>{
-    let o = JSON.parse(localStorage.getItem('but'));
-    if (o.length < 6) {
-        const storage = localStorage.getItem('but') || '[]'
-        const cart = JSON.parse(storage)
-        const card = {title}
-        localStorage.setItem('but', JSON.stringify([...cart,card]))
-//      item = title;
-//      tg.sendData(item);
-    }
-  })
-
-  btn.addEventListener("touchend", () =>{
-    let o = JSON.parse(localStorage.getItem('but'));
-    if (o.length < 6) {
-        const storage = localStorage.getItem('but') || '[]'
-        const cart = JSON.parse(storage)
-        const card = {title}
-        localStorage.setItem('but', JSON.stringify([...cart,card]))
-//      item = title;
-//      tg.sendData(item);
-    }
-  })
+  
 })
 
 
